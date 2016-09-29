b71559b27534491d433da365c11b86cd2993df8b
Your Name <you@example.com>
2016-09-29 16:11:51 +0800

    init

---
+++ index.js
@@ -0,0 +1,55 @@
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/mongoose-test');
+
+var db=mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // console.log('success!');
+  var userSchema = mongoose.Schema({
+      name: String,
+      pwd: String,
+      age: String
+  });
+  // var cat = mongoose.model('cat', catSchema);
+  //pppp 是实际数据库中记录的名字，此时记录在内存中
+  var User=mongoose.model('user',userSchema);
+  // var peter = new user({ name: 'HelloKitty',pwd: '123456', age: '22'});
+  // peter.save()//保存在数据库中
+  // console.log(peter.name,peter.pwd,peter.age)
+
+  //read
+  // console.log(user.find());
+  // user.find().exec(function(error,users){
+  //   console.log(users);
+  // })
+
+  //update
+  // peter.name='kong';
+  // peter.pwd='654321';
+  // peter.age='12';
+  // user.find().exec(function(error,users){
+  //   console.log(users);
+  // })
+
+// User.findById({_id: '57ecbaaaa6fed61806610614'},function(err,user){
+//   user.name='klj'
+//   user.save(function(err){
+//     console.log('更新了！');
+//     User.find().exec(function(error,users){
+//       console.log(users);
+//     })
+//   })
+// })
+// console.log('我先出来了');
+
+//删除
+User.findById({_id: '57ecbaaaa6fed61806610614'},function(err,user){
+  user.remove(function(err){
+    console.log('删除了！');
+    User.find().exec(function(error,users){
+      console.log(users);
+    })
+  })
+})
+});
