47aba69868af0ac2ff033d932a9559d6b2f723e9
小静 <18712799109@163.com>
2016-09-29 16:14:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -14,17 +14,18 @@ db.once('open', function() {
   // var cat = mongoose.model('cat', catSchema);
   //pppp 是实际数据库中记录的名字，此时记录在内存中
   var User=mongoose.model('user',userSchema);
+  //增加 
   // var peter = new user({ name: 'HelloKitty',pwd: '123456', age: '22'});
   // peter.save()//保存在数据库中
   // console.log(peter.name,peter.pwd,peter.age)
 
-  //read
+  //read 查询
   // console.log(user.find());
   // user.find().exec(function(error,users){
   //   console.log(users);
   // })
 
-  //update
+  //update 更新1
   // peter.name='kong';
   // peter.pwd='654321';
   // peter.age='12';
@@ -32,6 +33,7 @@ db.once('open', function() {
   //   console.log(users);
   // })
 
+  //update 更新2
 // User.findById({_id: '57ecbaaaa6fed61806610614'},function(err,user){
 //   user.name='klj'
 //   user.save(function(err){
